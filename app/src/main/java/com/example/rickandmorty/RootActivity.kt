bafecb16d6bb1bb52e472168ddcb7ac75622dea6
seropp <ts.seropps@gmail.com>
2022-04-22 17:22:50 +0300

    change activity name

--- app/src/main/java/com/example/rickandmorty/RootActivity.kt
+++ app/src/main/java/com/example/rickandmorty/RootActivity.kt
@@ -3,9 +3,9 @@ package com.example.rickandmorty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_root)
     }
 }
