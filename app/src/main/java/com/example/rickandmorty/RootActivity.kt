c7521422ef6fa74112cb1c4e0dede454bf2fb2c5
seropp <ts.seropps@gmail.com>
2022-04-22 18:23:41 +0300

    add splash screen 2 sec

--- app/src/main/java/com/example/rickandmorty/RootActivity.kt
+++ app/src/main/java/com/example/rickandmorty/RootActivity.kt
@@ -2,10 +2,22 @@ package com.example.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class RootActivity : AppCompatActivity() {
+
+    private val vm: SplashScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                vm.isLoading.value
+            }
+        }
+
         setContentView(R.layout.activity_root)
     }
 }
